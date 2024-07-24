f4aeaab12480e0c9a75967aa8041520f2ccdf1ee
CarlosEDsl <carloseduardoestds@gmail.com>
2024-07-24 16:53:40 -0300

    feat: header created

---
+++ src/app/shared/side-bar/side-bar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav'
+import {MatListModule} from '@angular/material/list'
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-side-bar',
+  standalone: true,
+  imports: [MatSidenavModule, MatListModule, MatIconModule, MatButtonModule, MatToolbarModule],
+  templateUrl: './side-bar.component.html',
+  styleUrl: './side-bar.component.scss'
+})
+
+export class SideBarComponent {
+
+}
