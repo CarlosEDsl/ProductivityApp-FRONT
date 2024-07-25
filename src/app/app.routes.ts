8fd38e86af3e95767aa0454cb9cd3370bac1a1ac
CarlosEDsl <carloseduardoestds@gmail.com>
2024-07-25 17:19:49 -0300

    ref: making 'menu button' on header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TaskPageComponent } from './features/tasks/task-page/task-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'my-tasks',
+    component:TaskPageComponent
+  }
+];
