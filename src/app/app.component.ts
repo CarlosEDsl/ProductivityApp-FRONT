dcd4e30df45191a407eaf96a442fd78b3379eb3f
CarlosEDsl <carloseduardoestds@gmail.com>
2024-07-26 15:02:01 -0300

    ref: sidebar remaked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideBarComponent } from './shared/side-bar/side-bar.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SideBarComponent, HeaderComponent],
+  imports: [RouterOutlet, SideBarComponent, HeaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ProductivityAPP-View';
+  isSideBarCompacted = false;
+
+  toggleSideBar() {
+    this.isSideBarCompacted = !this.isSideBarCompacted;
+  }
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -11,5 +11,11 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  @Output() toggleNav = new EventEmitter<void>();
+
+  onToggleNav() {
+    this.toggleNav.emit();
+  }
 
 }
