131eb127558ad85287017a4ecf5d0de00324702e
CarlosEDsl <carloseduardoestds@gmail.com>
2024-07-28 19:48:24 -0300

    feat: user routes connected and account creation linked

--- src/app/features/authentication/register/register.component.ts
+++ src/app/features/authentication/register/register.component.ts
@@ -1,25 +1,91 @@
-import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { User } from './../../../shared/interfaces/user';
+import { Component, ElementRef, inject, Renderer2, ViewChild, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormField } from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { UsersServiceService } from '../../../shared/services/users-service.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [MatFormField, MatInputModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
   @ViewChild('loginForm') loginForm!: ElementRef;
+  userService = inject(UsersServiceService);
+  @Input() user: User | null = null;
+  form!: FormGroup;
 
   constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      name: new FormControl<string>(this.user?.name ?? '', {
+        nonNullable: true,
+        validators: Validators.required
+      }),
+      cell: new FormControl<string>(this.user?.cell ?? '', {
+        nonNullable: false,
+        validators: Validators.required
+      }),
+      email: new FormControl<string>(this.user?.email ?? '', {
+        nonNullable: true,
+        validators: [Validators.required, Validators.email]
+      }),
+      confirmEmail: new FormControl<string>(this.user?.email ?? '', {
+        nonNullable: true,
+        validators: [Validators.required, Validators.email]
+      }),
+      password: new FormControl<string>(this.user?.password ?? '', {
+        nonNullable: true,
+        validators: Validators.required
+      }),
+      confirmPassword: new FormControl<string>(this.user?.password ?? '', {
+        nonNullable: true,
+        validators: Validators.required
+      })
+    });
+  }
 
   ngAfterViewInit(): void {
     if (this.loginForm) {
       setTimeout(() => {
         this.renderer.addClass(this.loginForm.nativeElement, 'show');
       }, 0);
+    }
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      let userFormRes = this.form.value;
+
+      if(userFormRes.email != userFormRes.confirmEmail){
+        console.error('Error registering user email dont match');
+      }
+      else if(userFormRes.password != userFormRes.confirmPassword){
+        console.error('Error registering user password dont match');
+      }
+      else {
+        const user:User = {
+          id: 0,
+          name: userFormRes.name,
+          email: userFormRes.email,
+          password: userFormRes.password,
+          cell: userFormRes.cell
+        }
+
+        this.userService.post(user).subscribe(
+          response => {
+            console.log('User registered successfully', response);
+          },
+          error => {
+            console.error('Error registering user', error);
+          }
+        );
+      }
     }
   }
 }
--- src/app/shared/services/users-service.service.ts
+++ src/app/shared/services/users-service.service.ts
@@ -1,7 +1,7 @@
-import { HoursTime } from '../interfaces/hours-time';
-import { inject, Injectable } from '@angular/core';
-import { User } from '../interfaces/user';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from '../interfaces/user';
+import { HoursTime } from '../interfaces/hours-time';
 
 @Injectable({
   providedIn: 'root'
@@ -9,39 +9,42 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class UsersServiceService {
 
   private ApiURL = 'http://localhost:8080';
-  users: User[] = [];
-  statsHour: HoursTime[] = []
 
-  httpClient = inject(HttpClient);
+  private httpClient = inject(HttpClient);
 
-  get(id:string) {
+  get(id: string) {
     return this.httpClient.get<User>(`${this.ApiURL}/user/${id}`);
   }
 
-  post(user:User) {
-    return this.httpClient.post(`${this.ApiURL}/user`, user);
+  post(user: User) {
+    return this.httpClient.post<User>(`${this.ApiURL}/user`, user, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      })
+    });
+
   }
 
-  put(user:User) {
-    return this.httpClient.put(`${this.ApiURL}/user/${user.id}`, user);
+  put(user: User) {
+    return this.httpClient.put<User>(`${this.ApiURL}/user/${user.id}`, user);
   }
 
-  delete(id:number, email:any) {
-    return this.httpClient.delete(`${this.ApiURL}/user/${id}`, email);
+  delete(id: number) {
+    return this.httpClient.delete(`${this.ApiURL}/user/${id}`);
   }
 
-  patch(id:number, statsHour:HoursTime) {
-    return this.httpClient.patch(`${this.ApiURL}/user/${id}`, statsHour);
+  patch(id: number, statsHour: HoursTime) {
+    return this.httpClient.patch<HoursTime>(`${this.ApiURL}/user/${id}`, statsHour);
   }
 
-  getMonthS(id:number, statsHour:HoursTime, auth:string) {
+  getMonthS(id: number, statsHour: HoursTime, auth: string) {
     return this.httpClient.request<HoursTime>('GET', `${this.ApiURL}/statistic/${id}`, {
       body: statsHour,
       headers: new HttpHeaders({
-        'Content-type': 'application/json',
-        'Authentication': auth
+        'Content-Type': 'application/json',
+        'Authorization': auth
       })
     });
   }
-
 }
