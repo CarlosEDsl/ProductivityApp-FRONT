6959b4a9dfb6596818c249d1e93208fa0ca3eb5d
CarlosEDsl <carloseduardoestds@gmail.com>
2024-08-07 14:48:47 -0300

    feat: paginator for tasks

--- src/app/features/tasks/task-page/task-page.component.ts
+++ src/app/features/tasks/task-page/task-page.component.ts
@@ -7,16 +7,19 @@ import { CommonModule } from '@angular/common';
 import { TokenServiceService } from '../../../shared/services/token-service.service';
 import { ActivatedRoute } from '@angular/router';
 import { CardComponent } from '../components/card/card.component';
+import {MatPaginatorModule, PageEvent} from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-task-page',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, CommonModule, CardComponent],
+  imports: [MatCardModule, MatButtonModule, CommonModule, CardComponent, MatPaginatorModule, MatFormFieldModule, MatInputModule],
   templateUrl: './task-page.component.html',
   styleUrl: './task-page.component.scss'
 })
 export class TaskPageComponent {
-  tasks:any = signal<Task[]>(inject(ActivatedRoute).snapshot.data['tasks']);
+  tasks: Task[] = inject(ActivatedRoute).snapshot.data['tasks'];
   taskService = inject(TaskService);
   authService:TokenServiceService = inject(TokenServiceService);
 
@@ -24,5 +27,30 @@ export class TaskPageComponent {
   trackByTaskTerm(index: number, task: Task): string {
     return task.term.toString();
   }
+
+  //Paginator
+  length = this.tasks.length;
+  pageSize = 3;
+  pageIndex = 0;
+
+  hidePageSize = true;
+  showPageSizeOptions = true;
+  showFirstLastButtons = true;
+  disabled = false;
+
+  pageEvent!: PageEvent;
+
+  handlePageEvent(e: PageEvent) {
+    this.pageEvent = e;
+    this.length = e.length;
+    this.pageSize = e.pageSize;
+    this.pageIndex = e.pageIndex;
+  }
+
+  get paginatedTasks(): Task[] {
+    const startIndex = this.pageIndex * this.pageSize;
+    return this.tasks.slice(startIndex, startIndex + this.pageSize);
+  }
+
 
 }
