e122c002961ce5285c872f4346991d2b756c6ec7
CarlosEDsl <carloseduardoestds@gmail.com>
2024-08-28 23:19:34 -0300

    feat: hours add to task creation

--- src/app/features/tasks/components/create/create.component.ts
+++ src/app/features/tasks/components/create/create.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, ChangeDetectionStrategy, ElementRef, ViewChild, Renderer2 } from '@angular/core';
+import { Component, inject, ChangeDetectionStrategy, ElementRef, ViewChild, Renderer2, ViewEncapsulation } from '@angular/core';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { TaskService } from '../../../../shared/services/task.service';
 import { Task } from '../../../../shared/interfaces/task';
@@ -7,12 +7,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatDatepicker, MatDatepickerModule} from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {provideNativeDateAdapter} from '@angular/material/core';
 import { TokenServiceService } from '../../../../shared/services/token-service.service';
-import { elementAt } from 'rxjs';
+import { MatBottomSheet, MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { NgxMaterialTimepickerComponent, NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
 
 
 @Component({
@@ -20,11 +21,12 @@ import { elementAt } from 'rxjs';
   standalone: true,
   imports: [MatDialogModule, MatButtonModule, MatIconModule,
     MatCardModule, CommonModule, FormsModule, ReactiveFormsModule, MatDatepickerModule,
-    MatInputModule, MatFormFieldModule],
+    MatInputModule, MatFormFieldModule, MatBottomSheetModule, NgxMaterialTimepickerModule],
     changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './create.component.html',
   providers: [provideNativeDateAdapter()],
-  styleUrl: './create.component.scss'
+  styleUrl: './create.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class CreateComponent {
   createDialog = inject(MatDialog);
@@ -32,12 +34,15 @@ export class CreateComponent {
   authService = inject(TokenServiceService);
   creationForm!: FormGroup;
   task?:Task;
+  private _bottomSheet = inject(MatBottomSheet);
 
   isEditingName: boolean = false;
   isEditingDescription: boolean = false;
   isEditingTerm: boolean = false;
 
   @ViewChild('nameInput') nameInput!: ElementRef;
+  @ViewChild(MatDatepicker) datePicker!: MatDatepicker<any>;
+  @ViewChild(NgxMaterialTimepickerComponent) timePicker!: NgxMaterialTimepickerComponent;
 
   constructor(private renderer:Renderer2) {}
 
@@ -52,11 +57,14 @@ export class CreateComponent {
       description: new FormControl(this.task?.description ?? '', {
         validators: Validators.maxLength(250)
       }),
-      term: new FormControl(this.task?.term ?? '', {
+      termDate: new FormControl(this.task?.term ?? '', {
         nonNullable: true,
         validators: Validators.required
-
-      })
+      }),
+      termTime: new FormControl('', {
+        nonNullable: true,
+        validators: Validators.required
+      }),
     });
   }
 
@@ -75,14 +83,20 @@ export class CreateComponent {
   onSubmit() {
     if (this.creationForm.valid) {
       const taskForm = this.creationForm.value;
+
+      const termDate = new Date(taskForm.termDate);
+
+      const {hour, minute} = this.brazillianHoursConverter(taskForm.termTime);
+      termDate.setHours(hour-3, minute, 0);
+
       const newTask: Task = {
         user_id: parseInt(this.authService.getId() || '0'),
         name: taskForm.name,
         description: taskForm.description,
-        term: new Date(taskForm.term).toISOString(),
+        term: termDate.toISOString(), // Combinação de data e hora
         inputDate: new Date().toISOString(),
         finishDate: taskForm.finishDate ? new Date(taskForm.finishDate).toISOString() : undefined
-      }
+      };
 
       if (newTask.user_id === 0) {
         throw new Error("Id not found");
@@ -98,10 +112,9 @@ export class CreateComponent {
           console.error('Error creating task', error);
         }
       });
-    } else {
-      this.renderer.addClass(this.nameInput, "incorrectInput")
     }
   }
+
 
 
   saveEdit(field: string) {
@@ -114,7 +127,38 @@ export class CreateComponent {
   }
 
   create() {
-    this.createDialog.open(CreateComponent);
+    this.createDialog.open(CreateComponent).addPanelClass("downDialog");
+  }
+
+  brazillianHoursConverter(time: string) {
+    let hour: number=0;
+    let minute: number;
+
+    // Extraia os valores de hora e minuto
+    const period = time.slice(-2); // "AM" ou "PM"
+    const timeWithoutPeriod = time.slice(0, -2); // Remove "AM" ou "PM"
+    const [hourString, minuteString] = timeWithoutPeriod.split(':');
+
+    // Converta os valores para números
+    const hourNumber = parseInt(hourString, 10);
+    minute = parseInt(minuteString, 10);
+
+    // Converta para o formato de 24 horas
+    if (period === 'PM') {
+      if (hourNumber !== 12) {
+        hour = hourNumber + 12; // Adiciona 12 horas para PM, exceto para 12 PM
+      } else {
+        hour = 12; // 12 PM é 12:00 no formato de 24 horas
+      }
+    } else if (period === 'AM') {
+      if (hourNumber === 12) {
+        hour = 0; // 12 AM é 00:00 no formato de 24 horas
+      } else {
+        hour = hourNumber;
+      }
+    }
+
+    return { hour, minute };
   }
 
 }
